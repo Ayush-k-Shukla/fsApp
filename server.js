1de3eaf64db3c0a9f0495289a3d01759613bc96c
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2022-01-25 10:28:34 +0530

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = process.env.PORT | 2000;
+const PORT = process.env.PORT || 2000;
 app.use(express.static("public"));
 app.use(express.json());
 const connectDB = require("./config/db");
