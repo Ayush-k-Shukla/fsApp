0ae85791cd7e5468ed6383f563cdc059e06d0930
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2022-01-26 11:03:33 +0530

    cors add

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 2000;
 app.use(express.static("public"));
 app.use(express.json());
 const connectDB = require("./config/db");
 connectDB();
+
+//cors
+const corsOptions = {
+  //give all name to allowing ports
+  origin: process.env.POSSIBLE_CLIENT.split(","),
+};
+app.use(cors(corsOptions));
 
 //template engine
 app.set("views", path.join(__dirname, "/views"));
